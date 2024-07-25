5809db037aaafe6853d785718d42d62573259619
ruslanrust <ruslanrust23@gmail.com>
2024-07-25 21:39:49 +0400

    Add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int age = 32;
+        string name = "Ruslan";
+        int temperatureInDegrees = 30;
+        bool isWomen = false;
+    }
+}
