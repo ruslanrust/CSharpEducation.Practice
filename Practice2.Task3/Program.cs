3b5e2ba589e7fb05a91d61f949aa5b7cd9844099
ruslanrust <ruslanrust23@gmail.com>
2024-07-27 21:48:03 +0400

    Add task 3 in practice 2

---
+++ Practice2.Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Practice2.Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите зачение переменной a");
+            double a = double.Parse(Console.ReadLine());
+
+            if (a == 0)
+            {
+                Console.WriteLine("На ноль делить нельзя");
+                return;
+            }
+            
+            Console.WriteLine("Введите зачение переменной b");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите зачение переменной f");
+            double f = double.Parse(Console.ReadLine());
+
+            double result = (a + b - f / a) + f * a * a - (a + b);
+            Console.WriteLine(result);
+
+        }
+    }
+}
