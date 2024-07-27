ef1d7f58ba5dac1ba784246e80cec5879e349461
ruslanrust <ruslanrust23@gmail.com>
2024-07-27 21:46:56 +0400

    Add task 2 in practice 2

---
+++ Practice2.Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Practice2.Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int meters = 5000;
+                int metersInKilometer = 1000;
+                int kilometers = meters / metersInKilometer;
+            }
+
+            {
+                int kilometers = 2;
+                int centimetersInKilometer = 100000;
+                int centimeters = kilometers * centimetersInKilometer;
+            }
+
+            {
+                double metersPerSecond = 5;
+                double kilometersPerHourInMetersPerSecond = 3.6;
+                double kilometersPerHour = metersPerSecond * kilometersPerHourInMetersPerSecond;
+            }
+
+            {
+                double degrees = 25;
+                double fahrenheit = (degrees * 9 / 5) + 32;
+            }
+        }
+    }
+}
