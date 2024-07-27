07ac53e26e124ef6dca815fc5592aa3f0ec6619a
ruslanrust <ruslanrust23@gmail.com>
2024-07-27 21:45:13 +0400

    Add task 1 in practice 2

---
+++ Practice2.Task1/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            int j = 0;
+            while (j < 5)
+            {
+                Console.WriteLine(j);
+                j += 1;
+            }
+
+            int k = 0;
+            do
+            {
+                Console.WriteLine(k);
+                k += 1;
+            } while (k < 5);
+
+            string result1 = "";
+
+            for (int i = 0; i < 3; i++)
+            {
+                string str = Console.ReadLine();
+                result1 = $"{result1}{str} ";
+            }
+            Console.WriteLine(result1);
+
+            string result2 = "";
+            int f = 0;
+            while (f < 3)
+            {
+                string str = Console.ReadLine();
+                result2 = $"{result2}{str} ";
+                f += 1;
+            }
+            Console.WriteLine(result2);
+
+            string result3 = "";
+            int l = 0;
+            do
+            {
+                string str = Console.ReadLine();
+                result3 = $"{result3}{str} ";
+                l += 1;
+            } while (l < 3);
+            Console.WriteLine(result3);
+
+
+        }
+    }
+}
