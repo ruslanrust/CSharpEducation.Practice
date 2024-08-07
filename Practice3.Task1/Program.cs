90c83eb83ceaa11c830673e785d9eef64082253b
ruslanrust <ruslanrust23@gmail.com>
2024-08-07 22:15:23 +0400

    Add task 1 in practice 3

---
+++ Practice3.Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Practice3.Task1
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Student brain = new Student();
+      brain.name = "Brain";
+      brain.age = 30;
+      brain.PrintInfo();
+    }
+  }
+}
---
+++ Practice3.Task1/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Practice3.Task1
+{
+  public class Student
+  {
+    public string name;
+    public int age;
+
+    public void PrintInfo()
+    {
+      Console.WriteLine($"Имя: {name}, возраст: {age}");
+    }
+  }
+}
