6ef72f05e4d67ed7f1f8df67b7614e53d5844348
ruslanrust <ruslanrust23@gmail.com>
2024-08-07 22:15:56 +0400

    Add task 2 in practice 3

---
+++ Practice3.Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Practice3.Task2
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Student brain = new Student();
+      Console.WriteLine(brain.name);
+      Console.WriteLine(brain.age);
+      Console.WriteLine(brain.averageScore);
+
+      brain.AverageScore = 2;
+      Console.WriteLine(brain.averageScore);
+
+    }
+
+  }
+}
---
+++ Practice3.Task2/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Practice3.Task2
+{
+  internal class Student
+  {
+    public string name;
+    public int age;
+    public double averageScore;
+
+    public double AverageScore
+    {
+      get
+      {
+        return this.averageScore;
+      }
+
+      set
+      {
+        if (value >= 0 && value <= 5)
+        {
+          this.averageScore = value;
+        }
+        else
+        {
+          throw new ArgumentException("Средний балл должен быть от 0 до 5");
+        }
+      }
+    }
+  }
+}
