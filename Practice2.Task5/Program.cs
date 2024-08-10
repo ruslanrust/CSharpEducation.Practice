75a352082879ab59267a9f380aca22808d0acaf1
ruslanrust <ruslanrust23@gmail.com>
2024-08-10 12:28:54 +0400

    Add task 5 in practice 2

---
+++ Practice2.Task5/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Practice2.Task5
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Введите первое число для сравнения");
+      int num1 = int.Parse(Console.ReadLine());
+   
+      Console.WriteLine("Введите второе число для сравнения");
+      int num2 = int.Parse(Console.ReadLine());
+
+
+      if (num1 > num2)
+      {
+        Console.WriteLine("Первое число больше второго");
+      }
+      else if (num1 < num2)
+      {
+        Console.WriteLine("Первое число меньше второго");
+      }
+      else
+      {
+        Console.WriteLine("Числа равны");
+      }
+    }
+  }
+}
