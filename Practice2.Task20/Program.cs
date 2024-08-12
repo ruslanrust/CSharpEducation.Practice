3e1e7a0607afd2a057d001bd83fca8db2d1e34fa
ruslanrust <ruslanrust23@gmail.com>
2024-08-12 22:10:21 +0400

    Add task 20 in practice 2

---
+++ Practice2.Task20/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Practice2.Task20
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      int[,] test = new int[3, 3];
+
+      Random randomNum = new Random();
+
+      int max = int.MinValue;
+      int min = int.MaxValue;
+
+      for (int i = 0; i < test.GetLength(0); i++)
+      {
+        for (int j = 0; j < test.GetLength(1); j++)
+        {
+          test[i, j] = randomNum.Next(1, 100);
+
+          if (test[i, j] > max)
+          {
+            max = test[i, j];
+          }
+
+          if (test[i, j] < min)
+          {
+            min = test[i, j];
+          }
+        }
+      }
+
+      Console.WriteLine(max - min);
+    }
+  }
+}
