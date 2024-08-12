1c5f22b106eab14b97f3336af3d5109f06ccea17
ruslanrust <ruslanrust23@gmail.com>
2024-08-12 22:11:16 +0400

    Add task 23 in practice 2

---
+++ Practice2.Task23/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Practice2.Task23
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Введите год: ");
+
+      int year = int.Parse(Console.ReadLine());
+
+      if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0)
+      {
+        Console.WriteLine("Високосный!");
+      }
+      else
+      {
+        Console.WriteLine("Невисокосный");
+      }
+    }
+  }
+}
