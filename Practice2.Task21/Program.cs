f1dabfe67155a8c3cb92ef12ea8952d7c8ac94cc
ruslanrust <ruslanrust23@gmail.com>
2024-08-12 22:10:38 +0400

    Add task 21 in practice 2

---
+++ Practice2.Task21/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Practice2.Task21
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      int lowerLimit = 1;
+      int upperLimit = 100;
+      int hiddenNum = new Random().Next(lowerLimit, upperLimit);
+
+      Console.Write($"Попробуйте угадать загаданное число от {lowerLimit} до {upperLimit}: ");
+
+      int answer = int.Parse(Console.ReadLine());
+
+      if (answer == hiddenNum)
+      {
+        Console.WriteLine("Угадали!");
+      }
+      else
+      {
+        Console.WriteLine("Не угадали!");
+      }
+      
+    }
+  }
+}
