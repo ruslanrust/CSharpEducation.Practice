dffe65135ea3e768b11e2e824028d745eb32e661
ruslanrust <ruslanrust23@gmail.com>
2024-08-12 21:54:02 +0400

    Add task 9 in practice 2

---
+++ Practice2.Task9/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Practice2.Task9
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      static int[] CreateArray (int length)
+      {
+        int[] result = new int[length];
+
+        for (int i = 0; i < length; i++)
+        {
+          result[i] = i;
+        }
+
+        return result;
+      }
+
+      static void PrintArray(int[] array)
+      {
+        foreach (int i in array)
+        {
+          Console.Write(i + " "); 
+        }
+      }
+
+      PrintArray(CreateArray(5));
+    }
+  }
+}
