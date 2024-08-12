8e9b7701a9d4d53386370845f50784e5cb44f667
ruslanrust <ruslanrust23@gmail.com>
2024-08-12 22:11:33 +0400

    Add task 24 in practice 2

---
+++ Practice2.Task24/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Practice2.Task24
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      foreach (Weekday weekday in Enum.GetValues(typeof(Weekday)))
+      {
+        Console.WriteLine(weekday);
+      }
+    }
+    enum Weekday
+    {
+      Monday,
+      Tuesday,
+      Wednesday,
+      Thursday,
+      Friday,
+      Saturday,
+      Sunday
+    }
+  }
+}
