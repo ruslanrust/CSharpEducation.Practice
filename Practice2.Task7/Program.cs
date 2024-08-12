161b324bead748543df9a7c5d8e89117b775b5f8
ruslanrust <ruslanrust23@gmail.com>
2024-08-12 21:53:15 +0400

    Add task 7 in practice 2

---
+++ Practice2.Task7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace Practice2.Task7
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      {
+        string str = "testForUP";
+        Console.WriteLine(str.ToUpper());
+      }
+
+      {
+        string str = "TESTfordown";
+        Console.WriteLine(str.ToLower());
+      }
+
+      {
+        string str = "привет";
+        Console.WriteLine(Char.ToUpper(str[0]) + str.Substring(1));
+      }
+    }
+  }
+}
