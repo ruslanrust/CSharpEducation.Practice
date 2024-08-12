917af56d18dc5f6d436fc2b3918b962fb0c559ab
ruslanrust <ruslanrust23@gmail.com>
2024-08-12 21:53:41 +0400

    Add task 8 in practice 2

---
+++ Practice2.Task8/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Practice2.Task8
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      int num1 = int.Parse(Console.ReadLine());
+      int num2 = int.Parse(Console.ReadLine());
+      int num3 = int.Parse(Console.ReadLine());
+
+      if (num1 == num2 || num1 == num3 || num2 == num3)
+      {
+        num1 += 5;
+        num2 += 5;
+        num3 += 5;
+      }
+      else
+      {
+        Console.WriteLine("Равных нет");
+      }
+    }
+  }
+}
