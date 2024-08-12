5433dcf961374e40f17c89bc1f479b32a9209be5
ruslanrust <ruslanrust23@gmail.com>
2024-08-12 22:04:25 +0400

    Add task 12 in practice 2

---
+++ Practice2.Task12/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Practice2.Task12
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      int length = 3;
+      string[] result = new string[length];
+
+      for (int i = 0; i < length; i++)
+      {
+        result[i] = Console.ReadLine();
+      }
+
+      foreach (string s in result)
+      {
+        Console.Write(s + " ");
+      }
+    }
+  }
+}
