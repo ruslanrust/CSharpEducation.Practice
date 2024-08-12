8918b1ea22923c23ffeadabba4c205568400ffb5
ruslanrust <ruslanrust23@gmail.com>
2024-08-12 22:03:45 +0400

    Add task 10 in practice 2

---
+++ Practice2.Task10/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Practice2.Task10
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      int length = int.Parse(args[0]);
+      int[] result = new int[length];
+
+      for (int i = 0; i < length; i++)
+      {
+        result[i] = i;
+      }
+        
+      foreach (int i in result)
+      {
+        Console.Write(i + " ");
+      }
+    }
+  }
+}
