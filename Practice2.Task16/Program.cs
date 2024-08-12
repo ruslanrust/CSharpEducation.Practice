a1c7787c0b54da6b2b1ccace20d6b5016cd4a0a1
ruslanrust <ruslanrust23@gmail.com>
2024-08-12 22:09:09 +0400

    Add task 16 in practice 2

---
+++ Practice2.Task16/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Practice2.Task16
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      int test = 3;
+
+      ChangeToOpposite(ref test);
+
+      Console.WriteLine(test);
+    }
+
+    static void ChangeToOpposite(ref int num)
+    {
+      num = num * (-1);
+    }
+  }
+}
