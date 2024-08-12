1a70b3c6ebe3675f44f1837b629f6a89513a160c
ruslanrust <ruslanrust23@gmail.com>
2024-08-12 22:09:28 +0400

    Add task 17 in practice 2

---
+++ Practice2.Task17/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Practice2.Task17
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      int num1 = 5;
+      int num2 = 3;
+      Console.WriteLine(num1 - num2);
+
+      ChangeVariables(ref num1, ref num2);
+
+      Console.WriteLine(num1 - num2);
+    }
+
+    static void ChangeVariables(ref int num1, ref int num2)
+    {
+      (num1, num2) = (num2, num1);
+    }
+  }
+}
