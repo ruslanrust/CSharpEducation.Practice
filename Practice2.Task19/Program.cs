cfcc48e60bd3ff59b9ee8130a098d2c7d8f9f826
ruslanrust <ruslanrust23@gmail.com>
2024-08-12 22:09:59 +0400

    Add task 19 in practice 2

---
+++ Practice2.Task19/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Practice2.Task19
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      int[] test = new int[10];
+      Random randomNum = new Random();
+
+      for (int i = 0; i < test.Length; i++)
+      {
+        test[i] = randomNum.Next(1, 100);
+      }
+
+      Console.WriteLine(test.Max() - test.Min());      
+    }
+  }
+}
