1dc7d553a3fbddaf18125dd09430db4346480e1f
ruslanrust <ruslanrust23@gmail.com>
2024-08-12 22:08:03 +0400

    Add task 13 in practice 2

---
+++ Practice2.Task13/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Practice2.Task13
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      int[,] test = MakeTwoDimArray(4, 5);
+      PrintTwoDimArray(test);
+    }
+
+    static int[,] MakeTwoDimArray(int rows, int columns) 
+    {
+      int[,] result = new int[rows, columns];
+      Random randomNum = new Random();
+
+      for (int i = 0; i < rows; i++)
+      {
+        for (int j = 0; j < columns; j++)
+        {
+          result[i, j] = randomNum.Next(1, 10);
+        }
+      }
+
+      return result;
+    }
+
+    static void PrintTwoDimArray(int[,] array)
+    {
+      for (int i = 0; i < array.GetLength(0); i++)
+      {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+          Console.Write($"{array[i, j]} ");
+        }
+
+        Console.WriteLine();
+      }
+    }
+  }
+}
