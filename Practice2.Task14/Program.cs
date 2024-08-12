c1be66d2875898a239b4972ada068986a4abc905
ruslanrust <ruslanrust23@gmail.com>
2024-08-12 22:08:33 +0400

    Add task 14 in practice 2

---
+++ Practice2.Task14/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Practice2.Task14
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      int length = 5;
+      int[] array = MakeArray(length);
+
+      Random randomNum = new Random();
+
+      for (int i = 0; i < array.Length; i++)
+      {
+        array[i] = randomNum.Next(1, 10);
+        Console.Write(array[i] + " ");
+      }
+    }
+
+    static int[] MakeArray(int length)
+    {
+      return new int[length];
+    }
+  }
+}
