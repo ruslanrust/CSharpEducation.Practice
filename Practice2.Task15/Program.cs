0888b9da9cc22152cb2f1d9311cf7a88a45497a4
ruslanrust <ruslanrust23@gmail.com>
2024-08-12 22:08:50 +0400

    Add task 15 in practice 2

---
+++ Practice2.Task15/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Practice2.Task15
+{
+  internal class Program
+  { 
+    static void Main(string[] args)
+    {
+      int[] test = new int[] { 1, 2, 3 };
+
+      ReverseArray(test);
+
+      foreach (int i in test)
+      {
+        Console.Write(i + " ");
+      }
+    }
+
+    static void ReverseArray(int[] array)
+    {
+      for (int i = 0; i < array.Length / 2; i++)
+      {
+        int temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+      }
+    }
+  }
+}
