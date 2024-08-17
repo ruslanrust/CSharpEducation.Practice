f0157065c3e9867db28d958e0b655c52c6a42ce2
ruslanrust <ruslanrust23@gmail.com>
2024-08-17 16:39:58 +0400

    Add task 4 in practice 3

---
+++ Practice3.Task4/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Practice3.Task4
+{
+  public class Book
+  {
+    public string title;
+    public string author;
+
+    public Book()
+    {
+      this.title = "Война и мир";
+      this.author = "Лев Толстой";
+    }
+
+    public Book(string title, string author)
+    {
+      this.title = title;
+      this.author = author;
+    }
+
+  }
+}
---
+++ Practice3.Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Practice3.Task4
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Book warAndPeace = new Book();
+      Book shogun = new Book("Сёгун", "Джеймс Клавелл");
+
+      Console.WriteLine(warAndPeace.title);
+      Console.WriteLine(warAndPeace.author);
+      Console.WriteLine(shogun.title);
+      Console.WriteLine(shogun.author);
+    }
+  }
+}
