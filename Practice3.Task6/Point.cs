52dfd4a28c99cf5b353539d2c9626c0f8efe1a91
ruslanrust <ruslanrust23@gmail.com>
2024-08-17 19:46:22 +0400

    Add task 6 in practice 3

---
+++ Practice3.Task6/Point.cs
@@ -0,0 +1,15 @@
+﻿namespace Practice3.Task6
+{
+  public struct Point
+  {
+    public int x;
+    public int y;
+
+    public Point (int x, int y)
+    {
+      this.x = x;
+      this.y = y;
+    }
+    
+  }
+}
---
+++ Practice3.Task6/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Practice3.Task6
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Point a = new Point(0, 0);
+      Point b = new Point(5, 5);
+
+      Console.WriteLine(CalculateDistance(a, b));
+    }
+
+    static public double CalculateDistance(Point a, Point b)
+    {
+      double dX = (double)(b.x - a.x);
+      double dY = (double)(b.y - a.y);
+
+      return Math.Sqrt(dX * dX + dY * dY);
+    }
+
+  }
+
+}
