be17cc93e372359ca5cd98d147e007f9c31dc198
ruslanrust <ruslanrust23@gmail.com>
2024-08-17 16:38:09 +0400

    Add task 3 in practice 3

---
+++ Practice3.Task3/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Practice3.Task3
+{
+  public class Calculator
+  {
+    public double Sum(double a, double b)
+    {
+      return a + b;
+    }
+
+    public double Substraction(double a, double b)
+    {
+      return a - b;
+    }
+
+    public double Division(double a, double b)
+    {
+      return a / b;
+    }
+
+    public double Multiplication(double a, double b)
+    {
+      return a * b;
+    }
+
+  }
+}
---
+++ Practice3.Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Practice3.Task3
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Calculator calculator = new Calculator();
+
+      Console.WriteLine(calculator.Sum(1, 1));
+      Console.WriteLine(calculator.Substraction(4, 2));
+      Console.WriteLine(calculator.Division(6, 3));
+      Console.WriteLine(calculator.Multiplication(1, 2));
+
+    }
+  }
+}
