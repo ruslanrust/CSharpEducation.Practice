24714fef9d3fd4b0df2ee73a1308931f1a28ed9e
ruslanrust <ruslanrust23@gmail.com>
2024-08-18 12:52:03 +0400

    Add task 9 in practice 3

---
+++ Practice3.Task9/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Practice3.Task9
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Student tom = new Student("Tom");
+      Console.WriteLine(tom.name);
+
+      Student.ChangeNameToAnonymous(tom);
+      Console.WriteLine(tom.name);
+    }
+  }
+}
---
+++ Practice3.Task9/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace Practice3.Task9
+{
+  public class Student
+  {
+    public string name;
+    public int age;
+
+    public Student(string name)
+    {
+      this.name = name;
+    }
+    public static void ChangeNameToAnonymous(Student student)
+    {
+      student.name = "Anonymous";
+    }
+  }
+}
