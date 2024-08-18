86c7ec19bb1a1786751e9070609169a433af631f
ruslanrust <ruslanrust23@gmail.com>
2024-08-18 21:27:59 +0400

    Add task 2 in practice 4

---
+++ Practice4.Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿using Practice4.Task1;
+
+namespace Practice4.Task2
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      MathHelper calculator = new MathHelper();
+
+      Console.WriteLine(calculator.Sum(1, 2));
+    }
+  }
+}
