b619884845eee636c2cc1b3886b6907a9fdcb308
ruslanrust <ruslanrust23@gmail.com>
2024-08-18 12:51:38 +0400

    Add task 8 in practice 3

---
+++ Practice3.Task8/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Practice3.Task8
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Rectangle rectnagle = new Rectangle(5, 5);
+      Console.WriteLine(Rectangle.CalculateArea(rectnagle));
+    }
+    
+  }
+}
---
+++ Practice3.Task8/Rectangle.cs
@@ -0,0 +1,19 @@
+﻿namespace Practice3.Task8
+{
+  public struct Rectangle
+  {
+    public double height;
+    public double width;
+
+    public Rectangle(double height, double width)
+    {
+      this.height = height;
+      this.width = width;
+    }
+
+    public static double CalculateArea(Rectangle rectanagle)
+    {
+      return rectanagle.height * rectanagle.width;
+    }
+  }
+}
