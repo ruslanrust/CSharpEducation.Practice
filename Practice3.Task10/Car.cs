702240c55fe716bdd6f6f4ff050a50ea3d3d041e
ruslanrust <ruslanrust23@gmail.com>
2024-08-18 12:52:18 +0400

    Add task 10 in practice 3

---
+++ Practice3.Task10/Car.cs
@@ -0,0 +1,16 @@
+﻿namespace Practice3.Task10
+{
+  public class Car
+  {
+    public string brand;
+
+    public Car(string brand)
+    {
+      this.brand = brand;
+    }
+    public static void PrintBrand(Car car)
+    {
+      Console.WriteLine("Марка: " + car.brand);
+    }
+  }
+}
---
+++ Practice3.Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Practice3.Task10
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Car audi = new Car("Audi");
+
+      Car.PrintBrand(audi);
+    }
+  }
+}
