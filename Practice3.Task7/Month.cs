8a49d3d4e031558e4b9f266a7610f15603727460
ruslanrust <ruslanrust23@gmail.com>
2024-08-18 10:35:41 +0400

    Add task 7 in practice 3

---
+++ Practice3.Task7/Month.cs
@@ -0,0 +1,18 @@
+﻿namespace Practice3.Task7
+{
+  public enum Month
+  {
+      January,
+      February,
+      March,
+      April,
+      May,
+      June,
+      Jule,
+      August,
+      September,
+      October,
+      November,
+      December
+  }
+}
---
+++ Practice3.Task7/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Practice3.Task7
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(CalculateDaysInMonth(Month.Jule));     
+    }
+
+    static int CalculateDaysInMonth(Month month)
+    {
+      switch (month)
+      {
+        case Month.January:
+        case Month.March:
+        case Month.May:
+        case Month.Jule:
+        case Month.August:
+        case Month.October:
+        case Month.December:
+          return 31;
+
+        case Month.April:
+        case Month.June:
+        case Month.September:
+        case Month.November:
+          return 30;
+
+        case Month.February:
+          return 28;
+
+        default:
+          return 0;
+      }
+      
+    }
+  }
+}
