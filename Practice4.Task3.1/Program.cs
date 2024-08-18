ca93b8381c5808df9bf63169064657491ddd87da
ruslanrust <ruslanrust23@gmail.com>
2024-08-18 21:40:04 +0400

    Add task 3 in practice 4

---
+++ Practice4.Task3.1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Practice4.Task3._1
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Person tom = new Person("Tom", 20);
+
+      Console.WriteLine(tom.GetInfo());
+    }
+  }
+}
---
+++ Practice4.Task3/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace Practice4.Task3
+{
+  public class Person
+  {
+    public string name;
+    public int age;
+
+    public Person(string name, int age)
+    {
+      this.name = name;
+      this.age = age;
+    }
+
+    public string GetInfo()
+    {
+      return $"Имя: {this.name}, возраст: {this.age}";
+    }
+  }
+}
