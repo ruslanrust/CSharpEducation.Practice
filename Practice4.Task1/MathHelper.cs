a4d36b519d75a794eb8f1a6992df8e7ee8d561f6
ruslanrust <ruslanrust23@gmail.com>
2024-08-18 21:27:40 +0400

    Add task 1 in practice 4

---
+++ Practice4.Task1/MathHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace Practice4.Task1
+{
+  public class MathHelper 
+  {
+    public double Sum(double a, double b)
+    {
+      return a + b;
+    }
+
+    public double Substraction(double a, double b)
+    {
+      return a - b;
+    }
+
+    public double Division(double a, double b)
+    {
+      return a / b;
+    }
+
+    public double Multiplication(double a, double b)
+    {
+      return a * b;
+    }
+  }
+}
