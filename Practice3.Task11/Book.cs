9a0b9db80d1de3a0bc7a68aca814d4816520c0bc
ruslanrust <ruslanrust23@gmail.com>
2024-08-18 12:52:33 +0400

    Add task 11 in practice 3

---
+++ Practice3.Task11/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace Practice3.Task11
+{
+  public class Book
+  {
+    public string title;
+    public string author;
+
+    public Book(string title, string author)
+    {
+      this.title = title;
+      this.author = author;
+    }
+
+    public static void PrintInfo(Book book)
+    {
+      Console.WriteLine($"Название: {book.title}, автор: {book.author}");
+    }
+  }
+}
---
+++ Practice3.Task11/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Practice3.Task11
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Book warAndPeace = new Book("Война и мир", "Лев Толстой");
+
+      Book.PrintInfo(warAndPeace);
+
+    }
+  }
+}
