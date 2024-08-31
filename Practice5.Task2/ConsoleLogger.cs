1686af31e79f942b491d59b35dbb2a271dba3c18
ruslanrust <ruslanrust23@gmail.com>
2024-08-31 13:48:36 +0400

    Add task 2 in practice 5

---
+++ Practice5.Task2/ConsoleLogger.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace Practice5.Task2
+{
+  public class ConsoleLogger : ILogger
+  {
+    public void Debug(string message)
+    {
+      Log(message, ILogger.LogLevel.Debug);
+    }
+
+    public void Error(string message)
+    {
+      Log(message, ILogger.LogLevel.Error); ;
+    }
+
+    public void Fatal(string message)
+    {
+      Log(message, ILogger.LogLevel.Fatal); ;
+    }
+
+    public void Info(string message)
+    {
+      Log(message, ILogger.LogLevel.Info);
+    }
+
+    public void Trace(string message)
+    {
+      Log(message, ILogger.LogLevel.Trace);
+    }
+
+    public void Warning(string message)
+    {
+      Log(message, ILogger.LogLevel.Warning);
+    }
+
+    public void Log(string message, ILogger.LogLevel logLevel)
+    {
+      StackTrace stacktrace = new StackTrace();
+      StackFrame prevframe = stacktrace.GetFrame(1);
+      MethodBase method = prevframe.GetMethod();
+      string className = method.ReflectedType.Name;
+
+      Console.WriteLine($"<{DateTime.Now}> | <{className}> | <{logLevel}> | <{message}>");
+    }
+  }
+}
---
+++ Practice5.Task2/ILogger.cs
@@ -0,0 +1,31 @@
+﻿namespace Practice5.Task2
+{
+  public interface ILogger
+  {
+    enum LogLevel
+    {
+      Trace,
+      Info,
+      Debug,
+      Warning,
+      Error,
+      Fatal,
+    }
+
+    void Trace(string message);
+
+    void Info(string message);
+
+    void Debug(string message);
+
+    void Warning(string message);
+
+    void Error(string message);
+
+    void Fatal(string message);
+
+    void Log(string message, LogLevel logLevel);
+  }
+
+
+}
